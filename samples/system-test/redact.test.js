66bd1078f6473e0001d9d8cfccc6dffd6b2b1235
Mike DaCosta <mikedacosta@google.com>
2018-06-21 11:54:47 -0700

    Fix tests.

--- samples/system-test/redact.test.js
+++ samples/system-test/redact.test.js
@@ -19,6 +19,8 @@ const path = require('path');
 const test = require('ava');
 const fs = require('fs');
 const tools = require('@google-cloud/nodejs-repo-tools');
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
 
 const cmd = 'node redact.js';
 const cwd = path.join(__dirname, `..`);
@@ -28,42 +30,69 @@ const testResourcePath = 'system-test/resources';
 
 test.before(tools.checkCredentials);
 
+function readImage(filePath) {
+  return new Promise((resolve, reject) => {
+    fs
+      .createReadStream(filePath)
+      .pipe(new PNG())
+      .on('error', reject)
+      .on('parsed', function() {
+        resolve(this);
+      });
+  });
+}
+
+async function getImageDiffPercentage(image1Path, image2Path) {
+  let image1 = await readImage(image1Path);
+  let image2 = await readImage(image2Path);
+  let diff = new PNG({width: image1.width, height: image1.height});
+
+  const diffPixels = pixelmatch(
+    image1.data,
+    image2.data,
+    diff.data,
+    image1.width,
+    image1.height
+  );
+  return diffPixels / (diff.width * diff.height);
+}
+
 // redact_image
 test(`should redact a single sensitive data type from an image`, async t => {
   const testName = `redact-single-type`;
   const output = await tools.runAsync(
-    `${cmd} image ${testImage} ${testName}.result.png -t PHONE_NUMBER`,
+    `${cmd} image ${testImage} ${testName}.actual.png -t PHONE_NUMBER`,
     cwd
   );
 
   t.regex(output, /Saved image redaction results to path/);
 
-  const correct = fs.readFileSync(
-    `${testResourcePath}/${testName}.correct.png`
+  const difference = await getImageDiffPercentage(
+    `${testName}.actual.png`,
+    `${testResourcePath}/${testName}.expected.png`
   );
-  const result = fs.readFileSync(`${testName}.result.png`);
-  t.deepEqual(correct, result);
+  t.true(difference < 0.03);
 });
 
 test(`should redact multiple sensitive data types from an image`, async t => {
   const testName = `redact-multiple-types`;
   const output = await tools.runAsync(
-    `${cmd} image ${testImage} ${testName}.result.png -t PHONE_NUMBER EMAIL_ADDRESS`,
+    `${cmd} image ${testImage} ${testName}.actual.png -t PHONE_NUMBER EMAIL_ADDRESS`,
     cwd
   );
 
   t.regex(output, /Saved image redaction results to path/);
 
-  const correct = fs.readFileSync(
-    `${testResourcePath}/${testName}.correct.png`
+  const difference = await getImageDiffPercentage(
+    `${testName}.actual.png`,
+    `${testResourcePath}/${testName}.expected.png`
   );
-  const result = fs.readFileSync(`${testName}.result.png`);
-  t.deepEqual(correct, result);
+  t.true(difference < 0.03);
 });
 
 test(`should report image redaction handling errors`, async t => {
   const output = await tools.runAsync(
-    `${cmd} image ${testImage} nonexistent.result.png -t BAD_TYPE`,
+    `${cmd} image ${testImage} output.png -t BAD_TYPE`,
     cwd
   );
   t.regex(output, /Error in redactImage/);
