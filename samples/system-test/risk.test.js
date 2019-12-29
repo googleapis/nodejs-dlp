03c7fdb212a5ce2e59a528a0bf4ee8edb5ff44a7
Justin Beckwith <beckwith@google.com>
2019-12-29 14:20:19 -0800

    fixy

--- samples/system-test/risk.test.js
+++ samples/system-test/risk.test.js
@@ -15,7 +15,7 @@
 'use strict';
 
 const {assert} = require('chai');
-const {describe, it} = require('mocha');
+const {describe, it, before, after} = require('mocha');
 const uuid = require('uuid');
 const {PubSub} = require(`@google-cloud/pubsub`);
 const cp = require('child_process');
